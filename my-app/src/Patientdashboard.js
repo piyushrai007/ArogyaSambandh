125d9574436c08e34dfb19ea01268bb2fc9077a6
Piyush Rai <111386563+piyushrai007@users.noreply.github.com>
2024-03-15 19:35:07 +0530

    Update Patientdashboard.js

--- my-app/src/Patientdashboard.js
+++ my-app/src/Patientdashboard.js
@@ -36,7 +36,7 @@ function AppointmentForm({ doctorId, doctorName, onAppointmentBooked }) {
         // Format the end time as HH:mm
         const endTime = `${adjustedEndHours < 10 ? '0' : ''}${adjustedEndHours}:${adjustedEndMinutes < 10 ? '0' : ''}${adjustedEndMinutes}`;
     
-        axios.post(' piyushrai.pythonanywhere.com/api/appointments/', {
+        axios.post('piyushrai.pythonanywhere.com/api/appointments/', {
             patient: userid,
             doctor: doctorId,
             speciality: specialty,
@@ -121,7 +121,7 @@ function DoctorList() {
     const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
 
     useEffect(() => {
-        axios.get(' piyushrai.pythonanywhere.com/doctors/', {
+        axios.get(' piyushrai.pythonanywhere.com/api/doctors/', {
             headers: {
                 'Authorization': `Bearer ${localStorage.getItem('access')}`
             }
